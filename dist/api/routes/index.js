33192eba9a4a9cb884b17d1267a57150c15bf093
Tony <tonysoto915@gmail.com>
2025-02-26 13:48:18 -0700

    delete nm

--- dist/api/routes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-//this is where we register our routes
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const users_1 = require("./users");
-const router = (0, express_1.Router)();
-router.use('/users', users_1.userRoutes);
-exports.default = router;
